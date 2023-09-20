8b0b7bb25bab5e5f83f57f027d67cf8b587acdaf
tonymushah <tonyramanantsalama@gmail.com>
2023-09-20 14:51:36 +0300

    init maven and spring

---
+++ src/main/java/mg/tonymushah/ejbtest/EjbTestApplication.java
@@ -0,0 +1,13 @@
+package mg.tonymushah.ejbtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjbTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjbTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mg/tonymushah/ejbtest/EjbTestApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.tonymushah.ejbtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjbTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
